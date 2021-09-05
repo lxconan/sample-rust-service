b96ef136cf237fead4a08cc789d7b6ca718e067c
Liu Xia <lxconan@gmail.com>
2021-09-06 07:37:31 +0800

    [luixia] feat: colorful output of the service install script.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-function Log {
+function Write-Log {
     param (
         [Parameter(Mandatory=$true)]
         [string]
@@ -8,6 +8,47 @@ function Log {
     Write-Host $Message -ForegroundColor Gray
 }
 
+function Write-Warning {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Message
+    )
+    
+    Write-Host $Message -ForegroundColor Yellow
+}
+
+function Write-Success {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Message
+    )
+
+    Write-Host $Message -ForegroundColor Green
+}
+
+function Write-Error {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Message
+    )
+
+    Write-Host $Message -ForegroundColor Red
+}
+
+function Install-Windows-Service {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ServiceName
+    )
+    
+    Write-Log -Message "We will now install windows service: $service_name"
+    
+}
+
 function Stop-Windows-Service {
     param (
         [Parameter(Mandatory=$true)]
@@ -15,28 +56,28 @@ function Stop-Windows-Service {
         $ServiceName
     )
 
-    Log -Message "Let's try stop the service first...";
+    Write-Log -Message "Let's try stop the service first..."
     Stop-Service -Name $ServiceName
 
     $is_service_stopped = $false;
     for ($i = 0; $i -lt 3; $i++) {
-        Log -Message "Wait until the service ($ServiceName) is stopped..."
+        Write-Log -Message "Wait until the service ($ServiceName) is stopped..."
         Start-Sleep -Seconds 1
         $service_controller = Get-Service -Name $service_name
         if ($null -eq $service_controller) {
-            Log -Message "The service ($ServiceName) is accidentially removed. Skip uninstalling..."
+            Write-Log -Message "The service ($ServiceName) is accidentially removed. Skip uninstalling..."
             return $true;
         }
 
         if ($service_controller.Status -eq 'Stopped') {
-            Log -Message "The service ($ServiceName) was stopped."
+            Write-Log -Message "The service ($ServiceName) was stopped."
             $is_service_stopped = $true;
             break;
         }
     }
 
     if (-not $is_service_stopped) {
-        Log -Message "The service ($ServiceName) cannot be stopped in certain time period."
+        Write-Log -Message "The service ($ServiceName) cannot be stopped in certain time period."
         return $false;
     }
 
@@ -45,16 +86,17 @@ function Stop-Windows-Service {
 
 $service_name = 'sample_service'
 
-Log -Message "Querying windows service installation status: $service_name..."
+Write-Log -Message "Querying windows service installation status: $service_name..."
 $query_service_result = Get-WmiObject win32_service | Where-Object{ $_.Name -eq $service_name };
-Log -Message "Querying windows service installation status: $service_name...Done"
+Write-Success -Message "Querying windows service installation status: $service_name...Done"
 
 if ($null -eq $query_service_result) {
-    Log -Message "The Windows service '$service_name' does not exist. No worries."
+    Write-Log -Message "The Windows service '$service_name' does not exist. No worries. Will install service."
 } else {
-    Log -Message "The Windows service '$service_name' currently exist. We need to uninstall the service."
+    Write-Log -Message "The Windows service '$service_name' currently exist. We need to uninstall the service."
     $stop_result = Stop-Windows-Service -ServiceName $service_name
     if (-not $stop_result) {
+        Write-Error -Message "The Windows service '$service_name' cannot be stopped. We will exit."
         return;
     }
 }
