fbebb6c3af2d7690bb69414fe422b27f63d80a41
Liu Xia <lxconan@gmail.com>
2021-09-06 09:07:56 +0800

    [liuxia] feat(service_installer): distinguish service installer command-line arguments.

--- installer/service-installer/src/main.rs
+++ installer/service-installer/src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Arg, App, ArgMatches};
+use colored::Colorize;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let matches:ArgMatches = App::new("Windows Service Installer")
+        .arg(
+            Arg::with_name("action type")
+                .short("a")
+                .long("action")
+                .required(true)
+                .multiple(false)
+                .possible_values(&["create", "remove"])
+                .takes_value(true)
+        )
+        .get_matches();
+
+    match matches.value_of("action type") {
+        None => {
+            // Will not be here actually.
+            exit(1);
+        }
+        Some(_) => {
+            exit(0);
+        }
+    }
 }
